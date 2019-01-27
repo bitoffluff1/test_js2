8f7e7feaaa9cce65141afe677be16fd16819d716
bitoffluff1 <Lena_khmelnikova@mail.ru>
2019-01-27 17:35:09 +0300

    Homework4.ex2

---
+++ L4/ex2/ex2.js
@@ -0,0 +1,53 @@
+var $button = document.getElementById("button");
+var $form = document.querySelectorAll("input");
+
+$button.addEventListener("click", handleButtonClick);
+
+function handleButtonClick() {
+    var name = $form[0].value;
+    var regName = /^[а-я\s]+$/i;
+    if (name.length > 0) {
+        if (regName.test(name) === false) {
+            $form[0].classList.add("error");
+            alert("Не правильно введена форма");
+        } else {
+            $form[0].classList.remove("error");
+        }
+    }
+
+
+    var phone = $form[1].value;
+    var regPhone = /^\+\d\(\d{3}\)\d{3}-\d{4}$/;
+    if (phone.length > 0) {
+        if (regPhone.test(phone) === false) {
+            $form[1].classList.add("error");
+            alert("Не правильно введена форма");
+        } else {
+            $form[1].classList.remove("error");
+        }
+    }
+
+    var email = $form[2].value;
+    var regEmail = /^.+@.+\..+$/;
+    if (email.length > 0) {
+        if (regEmail.test(email) === false) {
+            $form[2].classList.add("error");
+            alert("Не правильно введена форма");
+        } else {
+            $form[2].classList.remove("error");
+        }
+    }
+}
+
+(function ($) {
+    $.ajax({
+        url: "http://127.0.0.1:8080/L4/ex2/cities.json",
+        type: "POST",
+        dataType: "json",
+        success: function (data) {
+            $.each(data, function (i, elem) {
+                $("#cities").append($("<option value=" + elem.name + ">" + elem.name + "</option>"));
+            });
+        }
+    });
+})(jQuery);
